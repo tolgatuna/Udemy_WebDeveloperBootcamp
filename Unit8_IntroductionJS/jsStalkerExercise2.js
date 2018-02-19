e958e9b6b30d7b49b6e86ffe721dca4d7939c7ce
tolgatuna <tolga.tuna@hotmail.com>
2018-02-19 13:31:06 +0300

    "Chapter 8 - Introduction to JS" is completed.

---
+++ Unit8_IntroductionJS/jsStalkerExercise2.js
@@ -0,0 +1,4 @@
+var age = prompt("How old are you?");
+
+var days = age * 365;
+alert("You live roughly " + days + " days");
---
+++ Unit8_IntroductionJS/script.js
@@ -0,0 +1,3 @@
+var userName = prompt("What is your name?");	//Ask User Name
+alert("Hello to you " + userName);				// Show alert to user
+console.log("User Entered Name : " + userName); // Print userName to console 
